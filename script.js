f5d257d4f9a1b201ebf5a6e9bc8a55d03e96fecf
MajaNik86 <majanik86@gmail.com>
2023-03-06 12:15:07 +0100

    fetched quotes from an API

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('hello')
+let apiQuotes = [];
+
+
+//get qoutes from api:
+
+async function getQuotes() {
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        console.log(apiQuotes)
+        console.log(apiQuotes[12])
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+getQuotes();
